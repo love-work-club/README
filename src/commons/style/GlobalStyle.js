7f178d897a3ae9ef41a2e6679f233ceb0b14279c
sihyeon lee <hey210056@gmail.com>
2022-12-12 11:44:01 +0900

    [Feat]GlobalStyle 추가

---
+++ src/commons/style/GlobalStyle.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle, reset } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+${reset}
+*{
+    box-sizing: border-box;
+}
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './commons/style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <div>
+        <GlobalStyle />
         <App />
     </div>
 );
