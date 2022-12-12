e5f78b22a6e9d8dacc46bb4b6568d65f8d044819
TaeWon Park <thisis_tw94@naver.com>
2022-12-12 13:03:10 +0900

    [Feat]회원가입-html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import JoinMembership from './JoinMembership';
+import ProfileSetting from './ProfileSetting';
 
 function App() {
   return (
     <div>
+      <ProfileSetting />
       <JoinMembership />
     </div>
   );
