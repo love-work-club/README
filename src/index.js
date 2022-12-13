219f985d5899e11fc853d4935fa8395d4d828986
TaeWon Park <thisis_tw94@naver.com>
2022-12-13 09:08:48 +0900

    [Feat]회원가입- react(수정-route 삭제)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './commons/style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter>
+    <div>
         <GlobalStyles />
         <App />
-    </BrowserRouter>
+    </div>
 );
