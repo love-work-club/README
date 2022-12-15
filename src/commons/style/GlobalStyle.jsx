daeaed634b5a4631ae32768741b09683d09cdb6e
sihyeon lee <hey210056@gmail.com>
2022-12-15 14:12:05 +0900

    [Design]GlobalStyle 코드 수정

--- src/commons/style/GlobalStyle.jsx
+++ src/commons/style/GlobalStyle.jsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import pretendardRegular from '../../assets/font/Pretendard_Regular.woff2';
 import pretendardMedium from '../../assets/font/Pretendard_Medium.woff2';
 import pretendardBold from '../../assets/font/Pretendard_Bold.woff2';
@@ -22,19 +23,23 @@ const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Pretendard_Bold';
     font-style: normal;
-    font-weight: 500;
+    font-weight: 700;
     src: local('Pretendard_Bold'), url(${pretendardBold}) format('woff2');
 }
 
-
+${reset}
 * {
     box-sizing: border-box;
+}
+
+body {
     font-family: 'Pretendard_Regular';
     font-size: 14px;
     font-weight: 400;
 }
 
 button {
+    font-size: inherit;
     border: none;
     background-color: ${defaultTheme.palette.white}; 
 }
