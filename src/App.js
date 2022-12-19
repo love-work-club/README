f52a07f42777e4c0e8bbc144dce4799329a19eee
sihyeon lee <hey210056@gmail.com>
2022-12-19 17:51:31 +0900

    [Feat]라우터 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import EmailLogin from './pages/Login/EmailLogin';
+import Splash from './pages/Splash/Splash';
+import Home from './pages/Home/Home';
+import Chat from './pages/Chat/Chat';
+import Posts from './components/molecules/Posts/Posts';
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Splash />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/emailLogin" element={<EmailLogin />}></Route>
+
+                    <Route path="/home" element={<Home />}></Route>
+                    <Route path="/chat" element={<Chat />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 export default App;
