a873533baa0dbc4f0f74217d81ebeceead376d35
TaeWon Park <thisis_tw94@naver.com>
2022-12-20 16:19:35 +0900

    [Feat] YourProfile 4가지 버전 마크업 및 스타일링

--- src/pages/Profile/YourProfile.jsx
+++ src/pages/Profile/YourProfile.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import ProfileYourOrg from '../../components/organisms/YourProfile/ProfileYourOrg';
 import TopNavBarBasic from '../../components/molecules/TopNavBarBasic/TopNavBarBasic';
+import { Empty } from './Empty';
+import OnSale from './OnSale';
+import PostList from './PostList';
+import BottomNavBarBasic from '../../components/molecules/BottomNavBarBasic/BottomNavBarBasic';
 
-const YourProfileWrapper = styled.div`
-    width: 50%;
-    margin: 0 auto;
-`;
+const YourProfileWrapper = styled.div``;
 
 export default function YourProfile() {
     return (
         <YourProfileWrapper>
             <TopNavBarBasic />
             <ProfileYourOrg />
+            <Empty />
+            <OnSale />
+            <PostList />
+            <BottomNavBarBasic />
         </YourProfileWrapper>
     );
 }
