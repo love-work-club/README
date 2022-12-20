b9146ba952af1dec48c06681b558e816bc6c6814
TaeWon Park <thisis_tw94@naver.com>
2022-12-20 16:20:29 +0900

    [Feat] Route - /yourProfile 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './pages/Login/Login';
 import EmailLogin from './pages/Login/EmailLogin';
 import Splash from './pages/Splash/Splash';
 import Home from './pages/Home/Home';
+import YourProfile from './pages/Profile/YourProfile';
 import Chat from './pages/Chat/Chat';
 import Posts from './components/molecules/Posts/Posts';
 
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/emailLogin" element={<EmailLogin />}></Route>
 
                     <Route path="/home" element={<Home />}></Route>
+                    <Route path="/yourprofile" element={<YourProfile />}></Route>
                     <Route path="/chat" element={<Chat />}></Route>
                 </Routes>
             </BrowserRouter>
