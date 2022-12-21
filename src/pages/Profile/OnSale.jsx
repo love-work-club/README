e64d2c69074be09153fbb54a15d476605279f517
TaeWon Park <thisis_tw94@naver.com>
2022-12-21 23:16:58 +0900

    [Feat] 판매중인 상품 리스트 조회 기능구현

--- src/pages/Profile/OnSale.jsx
+++ src/pages/Profile/OnSale.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import ProductItem from '../../components/molecules/ProductItem/ProductItem';
-import SellingImg from '../../assets/images/selling-img.png';
 import defaultTheme from '../../commons/style/themes/default';
 
 const OnSaleWrapper = styled.div`
@@ -26,16 +26,51 @@ const TitleText = styled.h3`
 `;
 
 export default function OnSale() {
+    // 상품 리스트 불러오기
+    const [products, setProducts] = useState([]);
+    const API_HOST = 'https://mandarin.api.weniv.co.kr/';
+    const token =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOWM0MjViMTdhZTY2NjU4MWM2NTE4MCIsImV4cCI6MTY3Njc4NDU4NiwiaWF0IjoxNjcxNjAwNTg2fQ.-iJYh4ugLMcsAfzft3y2U6DyO2O3MVwYRG3Pq_DePto';
+
+    const config = {
+        method: 'get',
+        url: `${API_HOST}product/dotory`,
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+        },
+    };
+
+    useEffect(() => {
+        axios(config)
+            .then(res => {
+                setProducts(res.data.product);
+                console.log(res.data.product);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
-        <OnSaleWrapper>
-            <TitleText>판매 중인 상품</TitleText>
-            <ProductItemWrapper>
-                <ProductItem url={SellingImg} price="25,000원" />
-                <ProductItem url={SellingImg} price="11,500원" />
-                <ProductItem url={SellingImg} price="30,000원" />
-                <ProductItem url={SellingImg} price="24,000원" />
-                <ProductItem url={SellingImg} price="33,500원" />
-            </ProductItemWrapper>
-        </OnSaleWrapper>
+        <>
+            {products.length ? (
+                <OnSaleWrapper>
+                    <TitleText>판매 중인 상품</TitleText>
+                    <ProductItemWrapper>
+                        {products.length &&
+                            products.map((product, i) => (
+                                <ProductItem
+                                    key={i}
+                                    imgSrc={product.itemImage ? API_HOST + product.itemImage : null}
+                                    price={product.price}
+                                    itemName={product.itemName}
+                                />
+                            ))}
+                    </ProductItemWrapper>
+                </OnSaleWrapper>
+            ) : null}
+        </>
     );
 }
