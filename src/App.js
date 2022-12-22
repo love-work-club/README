46afbd3fad8b911a414a2264a82fd104a6023f13
sihyeon lee <hey210056@gmail.com>
2022-12-22 13:34:40 +0900

    [Add] 라우터 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import EmailLogin from './pages/Login/EmailLogin';
+import Join from './pages/Join/JoinMembership';
 import Splash from './pages/Splash/Splash';
 import Home from './pages/Home/Home';
 import YourProfile from './pages/Profile/YourProfile';
 import Chat from './pages/Chat/Chat';
-import Posts from './components/molecules/Posts/Posts';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
     return (
@@ -14,11 +15,12 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Splash />}></Route>
                     <Route path="/login" element={<Login />}></Route>
-                    <Route path="/emailLogin" element={<EmailLogin />}></Route>
-
+                    <Route path="/login/emailLogin" element={<EmailLogin />}></Route>
+                    <Route path="/login/join" element={<Join />}></Route>
                     <Route path="/home" element={<Home />}></Route>
                     <Route path="/yourprofile" element={<YourProfile />}></Route>
                     <Route path="/chat" element={<Chat />}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
