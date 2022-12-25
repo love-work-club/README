97e1745e9739c3437b023abf0fdbd61cd4fe35de
sihyeon lee <hey210056@gmail.com>
2022-12-25 21:40:43 +0900

    [Add] 라우터 로그인 여부 검사 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 import Login from './pages/Login/Login';
 import EmailLogin from './pages/Login/EmailLogin';
 import Join from './pages/Join/JoinMembership';
@@ -9,17 +11,27 @@ import Chat from './pages/Chat/Chat';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
+    const authCtx = useContext(AuthContext);
+
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Splash />}></Route>
-                    <Route path="/login" element={<Login />}></Route>
-                    <Route path="/login/emailLogin" element={<EmailLogin />}></Route>
-                    <Route path="/login/join" element={<Join />}></Route>
-                    <Route path="/home" element={<Home />}></Route>
-                    <Route path="/yourprofile" element={<YourProfile />}></Route>
-                    <Route path="/chat" element={<Chat />}></Route>
+                    {!authCtx.isLoggedIn && (
+                        <>
+                            <Route path="/login" element={<Login />}></Route>
+                            <Route path="/login/emailLogin" element={<EmailLogin />}></Route>
+                            <Route path="/login/join" element={<Join />}></Route>
+                        </>
+                    )}
+                    {authCtx.isLoggedIn && (
+                        <>
+                            <Route path="/home" element={<Home />}></Route>
+                            <Route path="/yourprofile" element={<YourProfile />}></Route>
+                            <Route path="/chat" element={<Chat />}></Route>
+                        </>
+                    )}
                     <Route path="*" element={<NotFound />}></Route>
                 </Routes>
             </BrowserRouter>
