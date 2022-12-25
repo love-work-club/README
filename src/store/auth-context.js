63b679046d722ca5927ff50e1e5e9dcc2482160b
sihyeon lee <hey210056@gmail.com>
2022-12-26 02:50:12 +0900

    [Modify] auth-context 파일 오타 수정

---
+++ src/store/auth-context.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const AuthContext = React.createContext({
+    token: '',
+    isLoggedIn: false,
+    login: toekn => {},
+});
+
+export const AuthContextProvider = ({ children }) => {
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken);
+
+    const userIsLoggedIn = !!token;
+
+    const loginHandler = userToken => {
+        setToken(userToken);
+        localStorage.setItem('token', userToken);
+    };
+
+    const contextValue = {
+        token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+    };
+
+    return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
