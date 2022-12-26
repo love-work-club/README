c986a961c3a663ab0b9df0596655e569b67c23fb
TaeWon Park <thisis_tw94@naver.com>
2022-12-26 21:17:43 +0900

    [Modify] dummy data 삭제

--- src/components/organisms/Profile/MyProfile/ProfileMyOrg.jsx
+++ src/components/organisms/Profile/MyProfile/ProfileMyOrg.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import ProfileImage from '../../../../assets/images/default_profile_user.svg';
 import FollowCount from '../../../molecules/FollowCount/FollowCount';
 import ProfileDsc from '../../../molecules/ProfileDsc/ProfileDsc';
 import ProfileImg from '../../../molecules/ProfileImg/ProfileImg';
 import ButtonGroupMy from '../../../molecules/ButtonGroupMy/ButtonGroupMy';
+import AuthContext from '../../../../store/auth-context';
 
 const ProfileMyWrapper = styled.div`
     width: 390px;
@@ -26,13 +26,12 @@ const CounterDiv = styled.div`
 export default function ProfileMyOrg() {
     // 나의 프로필 정보 가져오기
     const [profile, setProfile] = useState([]);
-    const API_HOST = 'https://mandarin.api.weniv.co.kr/';
-    const token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTQxNjkxMTdhZTY2NjU4MWVkZGRlYiIsImV4cCI6MTY3Njg4MjEyMSwiaWF0IjoxNjcxNjk4MTIxfQ.kOmzsz-l1wU5bZ0bl4R30u5SooaO22Xp8MgI2zJK82k';
+    const token = useContext(AuthContext).token;
+    const API_HOST = process.env.REACT_APP_BASE_URL;
 
     const config = {
         method: 'get',
-        url: `${API_HOST}user/myinfo`,
+        url: `${API_HOST}/user/myinfo`,
         headers: {
             Authorization: `Bearer ${token}`,
             'Content-type': 'application/json',
@@ -52,7 +51,7 @@ export default function ProfileMyOrg() {
         <ProfileMyWrapper>
             <CounterDiv>
                 <FollowCount count={profile.followerCount} kind="follower" />
-                <ProfileImg src={API_HOST + profile.image} alt="ProfileImg" />
+                <ProfileImg src={`${API_HOST}/${profile.image}`} alt="ProfileImg" />
                 <FollowCount count={profile.followingCount} kind="following" />
             </CounterDiv>
             <ProfileDsc username={profile.username} userId={profile.accountname} userDesc={profile.intro} />
