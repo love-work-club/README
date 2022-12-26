010d8f01339c96b1130061a5d6d6a9821d267b7b
TaeWon Park <thisis_tw94@naver.com>
2022-12-26 15:42:27 +0900

    [Feat] Portal.js 생성

---
+++ src/Portal.js
@@ -0,0 +1,7 @@
+import reactDom from 'react-dom';
+
+export default function ModalPortal({ children }) {
+    const el = document.getElementById('modal');
+
+    return reactDom.createPortal(children, el);
+}
