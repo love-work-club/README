8e5c348d612376c1412aa4f7a511ebb7b75021b9
TaeWon Park <thisis_tw94@naver.com>
2022-12-28 20:19:24 +0900

    [Feat] following 기능 구현 시작

--- src/components/molecules/ButtonGroupYour/ButtonGroupYour.jsx
+++ src/components/molecules/ButtonGroupYour/ButtonGroupYour.jsx
@@ -1,31 +1,76 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { useState } from 'react';
 import FeedComment from '../../../assets/icons/message-btn.svg';
 import ProfileShare from '../../../assets/icons/share-btn.svg';
+import AuthContext from '../../../store/auth-context';
 import Button from '../../atoms/Button/Button';
+import defaultTheme from '../../../commons/style/themes/default';
 
 const ButtonDiv = styled.div`
     display: flex;
     justify-content: center;
 `;
 
-export default function ButtonGroupYour() {
-    // 팔로우 버튼 클릭 시 팔로우 취소
-    const [follow, setFollow] = useState(false);
+export default function ButtonGroupYour({ profile, ...props }) {
+    // 팔로잉,언팔로잉 기능 구현
+    const API_HOST = process.env.REACT_APP_BASE_URL;
+    const token = useContext(AuthContext).token;
+    const [isFollow, setIsFollow] = useState(profile.isfollow);
+
+    const followConfig = {
+        method: 'post',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+        },
+        url: `${API_HOST}/profile/${profile.accountname}/follow`,
+        data: {},
+    };
+
+    const unfollowConfig = {
+        method: 'delete',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+        },
+        url: `${API_HOST}/profile/${profile.accountname}/unfollow`,
+    };
+
+    async function followReq() {
+        try {
+            let res = '';
+
+            if (isFollow) {
+                res = await axios(unfollowConfig);
+            } else {
+                res = await axios(followConfig);
+            }
+
+            console.log(res);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     return (
         <ButtonDiv>
-            <a href="/comment">
+            <Link to="/comment">
                 <img src={FeedComment} alt="" style={{ marginLeft: '91px' }} />
-            </a>
+            </Link>
             <Button
                 size="medium"
-                children={follow === true ? '언팔로우' : '팔로우'}
-                onClick={() => setFollow(!follow)}
+                backgroundColor={isFollow ? defaultTheme.palette.gray : defaultTheme.palette.black}
+                children={isFollow ? '취소' : '팔로우'}
+                onClick={() => {
+                    setIsFollow(!isFollow);
+                    followReq();
+                }}
             />
-            <a href="/profile">
+            <Link to="/profile">
                 <img src={ProfileShare} alt="profileShare" style={{ marginRight: '91px' }} />
-            </a>
+            </Link>
         </ButtonDiv>
     );
 }
