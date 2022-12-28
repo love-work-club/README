12bedd2fd611fa7a1f3d83cc375db293d7138022
minzyee <vvzmsqufvv@gmail.com>
2022-12-29 01:23:03 +0900

    [Modify] 코드 리팩토링

--- src/components/molecules/TopNavBarUserSearch/TopNavBarUserSearch.jsx
+++ src/components/molecules/TopNavBarUserSearch/TopNavBarUserSearch.jsx
@@ -1,77 +1,35 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import AuthContext from '../../../store/auth-context';
+import React, { useState, useEffect } from 'react';
 import BackBtn from '../../atoms/BackBtn/BackBtn';
 import { TopNavBarHeader } from '../../atoms/TopNavBarWrap/TopNavBarWrap';
 import { SearchUserForm, SearchUserInput } from './TopNavBarUserSearchStyle';
 
-function TopNavBarUserSearch({ userSearching, defaultValue }) {
+function TopNavBarUserSearch({ handleKeyword }) {
     const [keyword, setKeyword] = useState('');
-    const [searchData, setSearchData] = useState([]);
+    // const [searchData, setSearchData] = useState([]);
     // const AuthCtx = useContext(AuthContext);
 
     const handleInputUserSearch = e => {
         setKeyword(e.target.value);
     };
 
-    const handleSubmit = async () => {
-        // e.preventDefault();
-        // userSearching(keyword);
-        if (keyword !== '') {
-            await axios
-                .get(`https://mandarin.api.weniv.co.kr/user/searchuser/?keyword=${keyword}`, {
-                    headers: {
-                        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOWZmNmM5MTdhZTY2NjU4MWM3ODIwMSIsImV4cCI6MTY3NzIwNDY3OCwiaWF0IjoxNjcyMDIwNjc4fQ.QHbCaTnyqdUqTkOmVfOuliHMrB2JGLPlkbVDdaXyE4g`,
-                        'Content-type': 'application/json',
-                    },
-                })
-                .then(res => {
-                    setSearchData(res.data);
-                    // console.log('데이터', searchData);
-                })
-                .catch(error => {
-                    if (error.response) {
-                        console.log(error.response.data);
-                        console.log(error.response.status);
-                        console.log(error.response.headers);
-                    } else if (error.request) {
-                        console.log(error.request);
-                    } else {
-                        console.log('Error', error.message);
-                    }
-                    console.log(error.config);
-                });
-        }
-    };
-
-    const handleOnClick = () => {
-        console.log('나와라');
-    };
-
-    // KeyPress 이벤트 함수
-    const handleOnKeyPress = e => {
-        if (e.key === 'Enter') {
-            handleOnClick(); // enter 입력이 되면 클릭 이벤트 실행
-        }
-    };
+    // const handleOnClick = () => {
+    //     console.log('나와라');
+    // };
 
     useEffect(() => {
-        handleSubmit();
+        handleKeyword(keyword);
     }, [keyword]);
-    console.log('데이터', searchData);
 
     return (
         <TopNavBarHeader>
-            <SearchUserForm onSubmit={handleSubmit}>
+            <SearchUserForm>
                 <BackBtn />
                 <SearchUserInput
                     type="text"
                     placeholder="계정 검색"
                     id="keyword"
                     value={keyword}
-                    // defaultValue={defaultValue}
                     onChange={handleInputUserSearch}
-                    onKeyPress={handleOnKeyPress}
                 />
             </SearchUserForm>
         </TopNavBarHeader>
--- src/components/organisms/SearchUser/SearchUser.jsx
+++ src/components/organisms/SearchUser/SearchUser.jsx
@@ -1,25 +1,67 @@
-import React, { useState } from 'react';
-import SearchApi from '../../../Api/SearchApi';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import AuthContext from '../../../store/auth-context';
 import TopNavBarUserSearch from '../../molecules/TopNavBarUserSearch/TopNavBarUserSearch';
 import SearchUserListSection from '../../molecules/SearchUserListSection/SearchUserListSection';
 
 // 1. 우선 검색한 유저 정보 리스트로 불러오기
 // 2. 필터링해주기
 // 3. 불러온 유저 정보 화면에 뿌려주기
-function SearchUser(props) {
-    const [username, setUsername] = useState([]);
-    const [accountname, setAccountname] = useState([]);
 
-    const handleKeyword = async keyword => {
-        const result = await SearchApi(keyword);
+// SearchUser가 내가 키워드 입력해서 얻어온 유저 데이터를 갖고있을 것이다.
+function SearchUser() {
+    const [searchResult, setSearchResult] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const token = useContext(AuthContext).token;
 
-        setUsername(result); // username안에 전달받은 데이터가 들어갈 것이다.
+    const handleKeyword = userInput => {
+        setTimeout(() => {
+            setKeyword(userInput);
+        }, 1000);
+        console.log(keyword);
     };
+
+    const searchUserData = async () => {
+        if (keyword !== '') {
+            await axios
+                .get(`https://mandarin.api.weniv.co.kr/user/searchuser/?keyword=${keyword}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        'Content-type': 'application/json',
+                    },
+                })
+
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log('Error', error.message);
+                    }
+                    console.log(error.config);
+                });
+        }
+        // console.log('뭐가나올까: ', token);
+    };
+
+    // searchUserData가 내가 keyword 입력할 때마다 서버로부터 해당 검색 키워드와 일치하는 유저 데이터를 받아온다.
+    useEffect(() => {
+        searchUserData(keyword);
+        // console.log('뭐가나올까: ', keyword);
+    }, [keyword]);
 
     return (
         <>
-            <TopNavBarUserSearch userSearching={handleKeyword} />
-            <SearchUserListSection />
+            {/* 내가 인풋창에 keyword 검색하면 username 불러오기 */}
+            <TopNavBarUserSearch handleKeyword={handleKeyword} />
+            {/* username과 accountname 불러오기 */}
+            <SearchUserListSection searchResult={searchResult} />
         </>
     );
 }
