2eaed642a9e7e80dc6f81864bab1b2477b05e0c1
TaeWon Park <thisis_tw94@naver.com>
2023-01-01 13:07:21 +0900

    [Modify] useNavigate 중복 제거

--- src/components/atoms/BackBtn/BackBtn.jsx
+++ src/components/atoms/BackBtn/BackBtn.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 import IconBack from '../../../assets/icons/icon-back.svg';
 
 export const BackBtnIcon = styled.button`
