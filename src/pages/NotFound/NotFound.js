99ed48be27e0e8a46d6601b5f7d36159b44dee33
sihyeon lee <hey210056@gmail.com>
2023-01-03 23:17:58 +0900

    [Design] NotFound, Splash 페이지 마크업

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../../assets/logo/404.png';
+import defaulttheme from '../../commons/style/themes/default';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+`;
+
+const NotFoundImg = styled.img`
+    height: 150px;
+    width: 150px;
+    margin: 40px;
+`;
+
+const NotFoundH1 = styled.h1`
+    font-family: 'Pretendard_Bold';
+    font-size: 50px;
+    color: ${defaulttheme.palette.primary};
+`;
 
 export default function NotFound() {
-    return <div>NotFound</div>;
+    return (
+        <Wrapper>
+            <NotFoundImg src={Logo} alt="Not Found" />
+            <NotFoundH1>Not Found</NotFoundH1>
+        </Wrapper>
+    );
 }
--- src/pages/Splash/Splash.js
+++ src/pages/Splash/Splash.js
@@ -1,7 +1,35 @@
+import styled from 'styled-components';
+import Logo from '../../assets/logo/README_black.png';
+import defaulttheme from '../../commons/style/themes/default';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+`;
+
+const LogoImg = styled.img`
+    height: 150px;
+    width: 150px;
+    margin: 40px;
+`;
+
+const StyledH1 = styled.h1`
+    font-family: 'Pretendard_Bold';
+    font-size: 50px;
+    color: ${defaulttheme.palette.primary};
+`;
+
 export default function Splash() {
     return (
         <>
-            <h1>수정예정</h1>
+            <Wrapper>
+                <LogoImg src={Logo} alt="README 로고" />
+                <StyledH1>READ ME</StyledH1>
+            </Wrapper>
         </>
     );
 }
