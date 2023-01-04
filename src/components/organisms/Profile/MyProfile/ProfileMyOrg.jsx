7448ac1741164033e314321f3a135b7ed098365d
TaeWon Park <thisis_tw94@naver.com>
2023-01-04 13:33:46 +0900

    [Modify] 상단간격 padding 30으로 변경

--- src/components/organisms/Profile/MyProfile/ProfileMyOrg.jsx
+++ src/components/organisms/Profile/MyProfile/ProfileMyOrg.jsx
@@ -20,7 +20,7 @@ const CounterDiv = styled.div`
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    margin-top: 50px;
+    padding-top: 30px;
 `;
 
 export default function ProfileMyOrg() {
