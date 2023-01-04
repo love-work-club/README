0d73d57ba0ee017d1a6d8cda3fd1ce97bb76d67a
minzyee <vvzmsqufvv@gmail.com>
2023-01-04 16:22:52 +0900

    [Style] 스플래쉬 밀리초 변경

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -27,7 +27,7 @@ function Pages() {
             setLoading(true);
             setTimeout(() => {
                 setLoading(false);
-            }, 1500);
+            }, 2500);
         }
     }, []);
 
